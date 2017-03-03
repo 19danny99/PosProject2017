311bc25f408529e2cb2442553f0f5dfa4ddb29d1
danie <danie@10.24.15.161>
2017-03-03 10:17:50 +0100

    test123

---
+++ PosProject2017/src/controller/test.java
@@ -0,0 +1,11 @@
+package controller;
+
+public class test
+{
+
+	public test()
+	{
+		// TODO Auto-generated constructor stub
+	}
+
+}
---
+++ PosProject2017/src/controller/test300.java
@@ -0,0 +1,11 @@
+package controller;
+
+public class test300
+{
+
+	public test300()
+	{
+		// TODO Auto-generated constructor stub
+	}
+
+}
